c3601c637acfbd077f4f5833bc69264e2d49f88b
akeinLP <akeinbantin@gmail.com>
2025-10-26 11:38:51 -0500

    created basic class "ACCOUNT" and created "MAIN"

--- XYZ_Bank.cpp
+++ XYZ_Bank.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
+class Account
+{
+    private:
+        double Balance;
+    
+    public:
+        Account(double balance)
+        {
+            Balance = 0.0;
+        }
+        void setBalance();
+        double getBalance();
+        double deposit();
+        bool withdraw();
+};
+int main()
+{
+
+    cout << "-----Welcome XYZ Bank-----" << endl;
+
+
+return 0;
+}
